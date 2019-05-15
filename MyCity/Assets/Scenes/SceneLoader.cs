58331541e34de0408c43fd30c12852bac6f0dab0
cle236 <roman.culjak@gmail.com>
2019-05-15 01:53:03 +0100

    Almost final

---
+++ MyCity/Assets/Scenes/SceneLoader.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadCustardFactory(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    public void LoadBullring(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    public void LoadCustardAR(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    public void LoadBullringAr(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    public void LoadScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+}
